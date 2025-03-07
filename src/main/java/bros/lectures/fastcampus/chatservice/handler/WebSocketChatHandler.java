e7b4f2d4b44755b4cc680e80f3fbe47c0a95d3ec
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-03-07 18:23:56 +0900

    웹소켓으로 채팅 서버 구현

---
+++ src/main/java/bros/lectures/fastcampus/chatservice/handler/WebSocketChatHandler.java
@@ -0,0 +1,28 @@
+package bros.lectures.fastcampus.chatservice.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("{} connected", session.getId());
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("{} disconnected", session.getId());
+    }
+}
