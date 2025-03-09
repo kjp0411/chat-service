bf95da09126852ae46be6457f84701695f57e502
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-03-09 12:25:51 +0900

    username 표시

--- src/main/java/bros/lectures/fastcampus/chatservice/handler/WebSocketChatHandler.java
+++ src/main/java/bros/lectures/fastcampus/chatservice/handler/WebSocketChatHandler.java
@@ -1,5 +1,8 @@
 package bros.lectures.fastcampus.chatservice.handler;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -11,18 +14,34 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class WebSocketChatHandler extends TextWebSocketHandler {
 
+    final Map<String, WebSocketSession> webSocketSessionMap = new ConcurrentHashMap<>();
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("{} connected", session.getId());
+
+        this.webSocketSessionMap.put(session.getId(), session);
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        super.handleMessage(session, message);
+        log.info("{} sent {}", session.getId(), message.getPayload());
+
+        this.webSocketSessionMap.values().forEach(
+            webSocketSession -> {
+                try {
+                    webSocketSession.sendMessage(message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            );
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info("{} disconnected", session.getId());
+
+        this.webSocketSessionMap.remove(session.getId());
     }
 }
